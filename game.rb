4a8904379469bf248a5096baf11555f115e70536
msobczyk-x <m.sobczyk55@wp.pl>
2022-11-22 14:28:31 +0100

    poziom 2

--- game.rb
+++ game.rb
@@ -7,15 +7,24 @@ guesses = 0
 #game
 while game_state
     puts "Guess a number between 1 and 100".colorize(:yellow)
-    guess = gets.chomp.to_i
-    if guess == randomNumber
-        puts "You win!".colorize(:green)
-        game_state = false
-    elsif guess > randomNumber
-        puts "Too high!".colorize(:red)
-        guesses += 1
-    elsif guess < randomNumber
-        puts "Too low!".colorize(:light_blue)
-        guesses += 1
+    guess = gets.chomp
+    guessNumber = Integer(guess) rescue false
+    if guessNumber == false
+        if guess == "finish"
+            puts "You have exited the game".colorize(:red)
+            game_state = false
+        end
+    else
+        guess = guess.to_i
+        if guess == randomNumber
+            puts "You win!".colorize(:green)
+            game_state = false
+        elsif guess > randomNumber
+            puts "Too high!".colorize(:red)
+            guesses += 1
+        elsif guess < randomNumber
+            puts "Too low!".colorize(:light_blue)
+            guesses += 1
+        end
     end
     end
